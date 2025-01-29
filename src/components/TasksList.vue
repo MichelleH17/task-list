515306c0eaa9d688327d91222de92519df8456ee
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-29 12:02:05 +0100

    Add condition when there is no task

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,9 +1,12 @@
 <template>
   <section class="bg-blue-50 px-4 py-10">
     <div class="container-xl lg:container m-auto">
-      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
-        Total task: {{ tasks.length }}
-      </h2>
+      <span v-if="tasks.length === 0" class="text-center">
+        No tasks found
+      </span>
+      <span v-else class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Total tasks: {{ tasks.length }}
+      </span>
       <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
         <TaskCard v-for="task in tasks" :key="task.id" :task="task" />
       </div>
