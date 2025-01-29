4a9587ba8ac6e439e572f9a3b94bd3918ab40415
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-29 11:07:18 +0100

    Create new component TaskCard to show task and hours

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ task.name }} - {{ task.hoursToComplete }} hours
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Task } from '../types'
+
+defineProps<{
+  task: Task
+}>()
+</script>
