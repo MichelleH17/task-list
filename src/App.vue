862700fca8a2d057d0c0c8a880387b7370c5e803
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-29 11:40:20 +0100

    Add imports and component to show tasks

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
 import TaskForm from './components/TaskForm.vue'
+import TasksList from './components/TasksList.vue'
+import { ref } from 'vue';
+import type { Task } from './types';
+
+const tasks = ref<Task[]>([])
+
+const addTask = (task: Task) => {
+  tasks.value.push(task)
+}
 </script>
 
 <template>
   <main>
-    <TaskForm />
+    <TaskForm :tasks="tasks" @add-task="addTask" />
+    <TasksList :tasks="tasks" />
   </main>
 </template>
