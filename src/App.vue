602560828bba7c15cb1ccd332f219c0bd9021765
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-29 16:34:31 +0100

    Add delete-task to TasksList

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,15 @@ const tasks = ref<Task[]>([])
 const addTask = (task: Task) => {
   tasks.value.push(task)
 }
+
+const deleteTask = (id: number) => {
+  tasks.value = tasks.value.filter(task => task.id !== id)
+}
 </script>
 
 <template>
   <main>
     <TaskForm :tasks="tasks" @add-task="addTask" />
-    <TasksList :tasks="tasks" />
+    <TasksList :tasks="tasks" @delete-task="deleteTask" />
   </main>
 </template>
