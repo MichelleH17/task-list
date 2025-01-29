7e22652382ec242c509ee56f79706e2979d3e103
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-29 11:06:36 +0100

    Create new component TasksList to show tasks

---
+++ src/components/TasksList.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Total task: {{ tasks.length }}
+      </h2>
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
+        <TaskCard v-for="task in tasks" :key="task.id" :task="task" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { defineProps} from 'vue'
+import type { Task } from '../types'
+import TaskCard from './TaskCard.vue'
+
+defineProps<{
+  tasks: Task[]
+}>()
+</script>
