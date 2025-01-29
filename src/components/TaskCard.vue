c53ad5e62b98b587d1145f50a2a3ecbb8d550bc3
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-29 16:57:24 +0100

    Delete task, add data from the form to show in card

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    {{ task.name }} - {{ task.hoursToComplete }} hours
+  <div class="flex gap-4 max-w-sm m-auto bg-blue-50 py-10 w-full sm:w-sm my-12 rounded-xl shadow">
+    <p>{{ task.hoursToComplete }}h</p>
+    <p>{{ task.name }}</p>
+    <button @click="deleteTask" class="bg-blue-800 text-gray-50 px-2 p-0-5 rounded">x</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Task } from '../types'
 
-defineProps<{
+const props = defineProps<{
   task: Task
 }>()
+
+const emit = defineEmits<{
+  (event: 'delete-task', id: number): void
+}>()
+
+const deleteTask = () => {
+  emit('delete-task', props.task.id)
+}
 </script>
