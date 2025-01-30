6cee704febaaa10a1f1754a7b3480acad1a2ac54
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-30 11:19:01 +0100

    Add updatedTask

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import TaskForm from './components/TaskForm.vue'
 import TasksList from './components/TasksList.vue'
-import { ref } from 'vue';
 import type { Task } from './types';
 
 const tasks = ref<Task[]>([])
@@ -15,11 +15,21 @@ const addTask = (task: Task) => {
 const deleteTask = (id: number) => {
   tasks.value = tasks.value.filter(task => task.id !== id)
 }
+
+const updateTask = (updatedTask: Task) => {
+  const task = tasks.value.find(task => task.id === updatedTask.id)
+  if (task) {
+    task.name = updatedTask.name
+    task.hoursToComplete = updatedTask.hoursToComplete
+  }
+}
 </script>
 
 <template>
   <main>
     <TaskForm :tasks="tasks" @add-task="addTask" />
-    <TasksList :tasks="tasks" @delete-task="deleteTask" :last-task-name="lastTaskName" />
+    <TasksList :tasks="tasks" @delete-task="deleteTask"
+    @update-task="updateTask"
+    :last-task-name="lastTaskName" />
   </main>
 </template>
