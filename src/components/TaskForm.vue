ac8d267bccccb8f086c8f924ef7213f2a08fc39c
Michaela Havlíková <Michelle.h@seznam.cz>
2025-01-30 10:14:13 +0100

    Style form card

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,18 +1,30 @@
 <template>
-  <section class="bg-gray-100 px-4 py-10 w-full md:w-md mx-auto my-12 rounded-xl shadow">
+  <section class="bg-gray-100 px-4 py-10 md:max-w-md md:mx-auto mx-4 my-12 rounded-xl shadow">
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-3 text-center">Add Task</h2>
       <p class="text-center mb-6">Enter the task name</p>
       <form @submit.prevent="addTask" class="flex flex-col gap-1 max-w-sm m-auto">
-        <label for="newTask" class="font-semibold">
-          Task Name
-        </label>
-        <input type="text" id="newTask" name="newTask" v-model="newTask" class="bg-white border border-gray-200 rounded-md p-1 mb-3" />
-        <label for="hoursToComplete" class="font-semibold">
-          Hours to complete
-        </label>
-        <input type="number" id="hoursToComplete" name="hoursToComplete" v-model="hoursToComplete" :min="1" class="bg-white border border-gray-200 rounded-md p-1 mb-6" />
-        <button type="submit" class="bg-green-500 px-5 py-2 rounded-xl text-gray-50 hover:bg-green-600 font-semibold">
+        <label for="newTask" class="font-semibold"> Task Name </label>
+        <input
+          type="text"
+          id="newTask"
+          name="newTask"
+          v-model="newTask"
+          class="bg-white border border-gray-200 rounded-md p-1 mb-3"
+        />
+        <label for="hoursToComplete" class="font-semibold"> Hours to complete </label>
+        <input
+          type="number"
+          id="hoursToComplete"
+          name="hoursToComplete"
+          v-model="hoursToComplete"
+          :min="1"
+          class="bg-white border border-gray-200 rounded-md p-1 mb-6"
+        />
+        <button
+          type="submit"
+          class="bg-green-500 px-5 py-2 rounded-xl text-gray-50 hover:bg-green-600 font-semibold"
+        >
           Add Task
         </button>
       </form>
@@ -40,7 +52,7 @@ const addTask = () => {
     const task: Task = {
       id: props.tasks.length + 1,
       name: newTask.value,
-      hoursToComplete: hoursToComplete.value
+      hoursToComplete: hoursToComplete.value,
     }
     emit('add-task', task)
     newTask.value = ''
